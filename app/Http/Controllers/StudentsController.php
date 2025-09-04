b64727808e511c1d8789a992eaef9a514a9c3986
paraiso3d <normanparaiso.abm12@gmail.com>
2025-09-05 03:33:13 +0800

    a

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -122,12 +122,12 @@ class StudentsController extends Controller
                 ->latest()
                 ->first();
 
-            if (!$enrollment) {
-                return response()->json([
-                    'isSuccess' => false,
-                    'message' => 'No enrollment record found.'
-                ], 404);
-            }
+            // if (!$enrollment) {
+            //     return response()->json([
+            //         'isSuccess' => false,
+            //         'message' => 'No enrollment record found.'
+            //     ], 404);
+            // }
 
             // Get payments
             $payments = payments::where('student_id', $student->id)
