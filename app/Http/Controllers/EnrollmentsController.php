123f4bd429dc9b82dc07f263f73155ac601483fb
paraiso3d <normanparaiso.abm12@gmail.com>
2025-12-11 01:41:42 +0800

    AAAAAA

--- app/Http/Controllers/EnrollmentsController.php
+++ app/Http/Controllers/EnrollmentsController.php
@@ -109,7 +109,8 @@ class EnrollmentsController extends Controller
             // Fetch schedules with applicant/admission details
             $query = exam_schedules::with([
                 'applicant.academic_program:id,course_name',
-                'applicant.school_years:id,school_year,semester',
+
+                'academicYear:id,school_year,semester', // load via exam_schedules relationship
                 'room:id,room_name',
                 'building:id,building_name',
                 'campus:id,campus_name'
@@ -140,11 +141,15 @@ class EnrollmentsController extends Controller
 
             $schedules = $query->paginate($perPage, ['*'], 'page', $page);
 
+
             // Transform into flat list of students + exam info
             $data = $schedules->map(function ($schedule) {
                 $admission = $schedule->applicant;
                 $storagePath = 'storage/';
 
+
+
+                $schoolYear = $schedule->academicYear;
                 return [
                     // Exam Schedule Details
                     'id'       => $schedule->id,
@@ -162,8 +167,8 @@ class EnrollmentsController extends Controller
                     // Full Admission Details
                     'admission_id'      => $admission->id ?? null,
                     'academic_year_id'    => $admission->academic_year_id ?? null,
-                    'school_year' =>    optional($admission->school_years)->school_year ?? null,
-                    'semester'    =>    optional($admission->school_years)->semester ?? null,
+                    'school_year' => optional($schoolYear)->school_year,
+                    'semester' => optional($schoolYear)->semester,
                     'first_name'        => $admission->first_name ?? null,
                     'middle_name'       => $admission->middle_name ?? null,
                     'last_name'         => $admission->last_name ?? null,
@@ -281,6 +286,8 @@ class EnrollmentsController extends Controller
 
 
             $query->orderBy('created_at', 'desc');
+
+
 
             $schedules = $query->paginate($perPage, ['*'], 'page', $page);
 
