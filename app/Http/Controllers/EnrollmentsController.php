e4cf981d2e7e7d6d4b7543cd1a0925ea763222e5
paraiso3d <normanparaiso.abm12@gmail.com>
2025-12-11 01:25:44 +0800

    AA

--- app/Http/Controllers/EnrollmentsController.php
+++ app/Http/Controllers/EnrollmentsController.php
@@ -162,8 +162,8 @@ class EnrollmentsController extends Controller
                     // Full Admission Details
                     'admission_id'      => $admission->id ?? null,
                     'academic_year_id'    => $admission->academic_year_id ?? null,
-                    'school_year' => optional($admission->academic_year_id)->school_year ?? null,
-                    'semester'    => optional($admission->academic_year_id)->semester ?? null,
+                    'school_year' =>    optional($admission->schoolYear)->school_year ?? null,
+                    'semester'    =>    optional($admission->schoolYear)->semester ?? null,
                     'first_name'        => $admission->first_name ?? null,
                     'middle_name'       => $admission->middle_name ?? null,
                     'last_name'         => $admission->last_name ?? null,
